8b8733a8051142ee2cb3198256ce9a7e3e235f60
soopeach <hsjeon584@gmail.com>
2023-04-07 02:20:36 +0900

    refactor: Client를 주입받아 사용하도록 변경

---
+++ data/src/main/java/com/pl/data/ClientModule.kt
@@ -0,0 +1,24 @@
+package com.pl.data
+
+import com.pl.domain.StatusBoardClient
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import io.ktor.client.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.serialization.kotlinx.json.*
+
+@Module
+@InstallIn(SingletonComponent::class)
+class ClientModule {
+
+    @Provides
+    fun provideClient(): StatusBoardClient {
+        return  StatusBoardClientImpl()
+    }
+
+}
--- data/src/main/java/com/pl/data/StatusBoardClientImpl.kt
+++ data/src/main/java/com/pl/data/StatusBoardClientImpl.kt
@@ -1,5 +1,6 @@
 package com.pl.data
 
+import com.pl.domain.StatusBoardClient
 import com.pl.domain.WebHookMessage
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
@@ -8,16 +9,15 @@ import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 
-class StatusBoardClient {
+class StatusBoardClientImpl: StatusBoardClient {
 
     private val client = HttpClient(CIO) {
         install(ContentNegotiation) {
             json()
         }
-
     }
 
-    suspend fun postWebHookMessage(message: WebHookMessage) {
+    override suspend fun postWebHookMessage(message: WebHookMessage) {
         client
             .post(BuildConfig.webhookUrl) {
                 header(HttpHeaders.ContentType, ContentType.Application.Json)
