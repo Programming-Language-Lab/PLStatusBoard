9917837aac5e3ca6d4c3cafa238742b7f0085a2d
soopeach <hsjeon584@gmail.com>
2023-05-02 21:26:38 +0900

    feat: 멤버의 상태를 서버에서 Reactive하게 가져오는 유즈케이스 추가

--- data/src/main/java/com/pl/data/StatusBoardClientImpl.kt
+++ data/src/main/java/com/pl/data/StatusBoardClientImpl.kt
@@ -1,7 +1,11 @@
 package com.pl.data
 
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.snapshots
+import com.google.firebase.ktx.Firebase
 import com.pl.data.model.firebase.FireStoreDocumentResponse
 import com.pl.data.model.firebase.FireStoreMembersFields
+import com.pl.data.model.firebase.MemberStateResponse
 import com.pl.domain.MemberState
 import com.pl.domain.StatusBoardClient
 import com.pl.domain.WebHookMessage
@@ -12,6 +16,7 @@ import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
+import kotlinx.coroutines.flow.map
 
 class StatusBoardClientImpl: StatusBoardClient {
 
@@ -35,6 +40,14 @@ class StatusBoardClientImpl: StatusBoardClient {
         }.body<FireStoreDocumentResponse<FireStoreMembersFields>>()
         return result.toMemberState()
     }
+
+    override fun getMemberStateFlow(name: String) =
+        Firebase.firestore.collection(DATA_BASE_NAME)
+        .document(name)
+        .snapshots()
+            .map {
+                it.toObject(MemberStateResponse::class.java)?.toMemberState() ?: MemberState.error()
+            }
 
     companion object {
         private const val FIRE_STORE_END_POINT = "https://firestore.googleapis.com/v1"
--- domain/src/main/java/com/pl/domain/StatusBoardClient.kt
+++ domain/src/main/java/com/pl/domain/StatusBoardClient.kt
@@ -1,7 +1,11 @@
 package com.pl.domain
 
+import kotlinx.coroutines.flow.Flow
+
 interface StatusBoardClient {
     suspend fun postWebHookMessage(message: WebHookMessage)
 
     suspend fun getMemberState(name: String): MemberState
+
+    fun getMemberStateFlow(name: String): Flow<MemberState>
 }
