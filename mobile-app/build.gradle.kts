a5e0842ca713d3b6ba9fbebc4ba134bba208d27d
soopeach <hsjeon584@gmail.com>
2023-05-03 15:54:51 +0900

    chore: mobile-app 모듈에 hilt viewModel 및 navigation 관련 의존성 추가

--- mobile-app/build.gradle.kts
+++ mobile-app/build.gradle.kts
@@ -75,6 +75,8 @@ dependencies {
 
     implementation("com.google.dagger:hilt-android:2.45")
     kapt("com.google.dagger:hilt-android-compiler:2.45")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.navigation:navigation-compose:2.5.3")
 }
 
 kapt {
