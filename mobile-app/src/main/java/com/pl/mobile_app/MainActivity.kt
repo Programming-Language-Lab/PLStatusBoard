3a960cb7cfe300915e8ba655165e7bf45d12f627
soopeach <hsjeon584@gmail.com>
2023-05-06 16:22:03 +0900

    feat: 원격으로 멤버의 상태를 변경할 수 있는 기능 구현

--- mobile-app/src/main/java/com/pl/mobile_app/MainActivity.kt
+++ mobile-app/src/main/java/com/pl/mobile_app/MainActivity.kt
@@ -3,14 +3,22 @@ package com.pl.mobile_app
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.pl.domain.MemberInfo
+import com.pl.domain.MemberState
+import com.pl.domain.MemberStatus
 import com.pl.mobile_app.ui.theme.PLStatusBoardTheme
 import com.pl.mobile_app.viewmodel.MainModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -25,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
@@ -33,17 +41,48 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier, vm: MainModel = hiltViewModel()) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MainScreen() {
+
+    val viewModel = hiltViewModel<MainModel>()
+
+    LazyColumn(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceEvenly,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        MemberInfo.allKorean().forEach { koreanName ->
+
+            item {
+                Text(text = koreanName)
+            }
+            item {
+                LazyRow(
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                ) {
+                    MemberStatus.allNormalStatus().forEach { memberStatus ->
+                        item {
+                            SimpleStateButton(memberStatus) {
+                                viewModel.postMemberState(
+                                    MemberState(
+                                        koreanName,
+                                        memberStatus
+                                    )
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    PLStatusBoardTheme {
-        Greeting("Android")
+fun SimpleStateButton(
+    memberStatus: MemberStatus,
+    onClicked: () -> Unit = {},
+) {
+    Button(onClick = { onClicked() }) {
+        Text(text = memberStatus.text)
     }
 }
