b518aa8a900fcaaec910dfa055eced8604b215bf
soopeach <hsjeon584@gmail.com>
2023-05-11 14:49:46 +0900

    fix: 테블릿에서 상태 변경하면 메시지가 가지 않던 이슈 해결

--- domain/src/main/java/com/pl/domain/usecase/SetMemberStateUseCase.kt
+++ domain/src/main/java/com/pl/domain/usecase/SetMemberStateUseCase.kt
@@ -7,12 +7,10 @@ import javax.inject.Inject
 class SetMemberStateUseCase @Inject constructor(
     private val statusBoardClient: StatusBoardClient,
     private val getMemberStateFromCacheUseCase: GetMemberStateFromCacheUseCase,
-    private val setMemberStateToCacheUseCase: SetMemberStateToCacheUseCase
 ) {
     suspend operator fun invoke(memberState: MemberState): Boolean {
         val cachedMemberState = getMemberStateFromCacheUseCase(memberState.name)
         return if (cachedMemberState != memberState.status.text) {
-            setMemberStateToCacheUseCase(memberState)
             statusBoardClient.setMemberState(memberState)
             true
         } else {
